44b8aa899e455c48d9e1104f47fac57202bfc359
Mike Schutte <tmikeschutte@gmail.com>
2017-05-19 22:35:19 -0600

    Refactor admin component

--- src/Components/Admin/Admin.js
+++ src/Components/Admin/Admin.js
@@ -1,40 +1,49 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
+import axios from 'axios'
 import './_admin.sass';
 
-class Admin extends Component {
+export default class Admin extends Component {
   constructor(){
     super();
     this.state = {cohorts: []};
   }
 
-  componentDidMount(){
-    this.retrieveCohorts();
+  async componentDidMount(){
+    const data = await this.retrieveCohorts(axios);
+    this.setState({ cohorts: data })
   }
 
-  retrieveCohorts(){
-    var that = this;
-    fetch('https://johariwindowapi.herokuapp.com/api/v1/cohorts')
-      .then(result => result.json())
-      .then(data => {
-        that.setState({ cohorts: data })
-        return true
-    })
+  async retrieveCohorts(conn){
+    try {
+      const response = await conn.get(
+        'https://johariwindowapi.herokuapp.com/api/v1/cohorts'
+      )
+      return response.data
+    } catch(error) {
+      console.error(error)
+    }
   }
 
   eachCohort(cohort, i){
-    return <Link key={i} to={'/admin/cohort/' + cohort.id }>{cohort.name}</Link>
+    return (
+      <Link key={i} to={'/admin/cohort/' + cohort.id}>
+        {cohort.name}
+      </Link>
+    )
   }
+
   render() {
+    const cohorts = this.state.cohorts.map(this.eachCohort)
+
     return (
       <div className='Admin'>
         <h1>Select a cohort to create groups.</h1>
         <div className='cohort-list'>
-          { this.state.cohorts.map(this.eachCohort) }
+          { cohorts }
         </div>
       </div>
     );
   }
 }
 
-export default Admin;
--- src/Components/Admin/Admin.test.js
+++ src/Components/Admin/Admin.test.js
@@ -4,14 +4,42 @@ import Admin from './Admin';
 import { BrowserRouter as Router } from 'react-router-dom';
 
 describe('Admin', () => {
-  let wrapper;
-
-  beforeEach(()=>{
-    wrapper = mount(<Router><Admin /></Router>)
-  })
+  const fakeAxios = {
+    get(url) {
+      return Promise.resolve({
+        data: [
+          {
+            id: 1,
+            name: "1610-BE"
+          }
+        ]
+      })
+    }
+  }
 
   it('renders admin page with links of cohorts', () => {
+    const restore = Admin.prototype.retrieveCohorts
+    const mock = Admin.prototype.retrieveCohorts = jest.fn()
+    const wrapper = mount(<Router><Admin conn={fakeAxios} /></Router>)
     expect(wrapper.find('.Admin').length).toEqual(1)
     expect(wrapper.find('.cohort-list').length).toEqual(1)
+    Admin.prototype.retrieveCohorts = restore
+  })
+
+  describe('#retrieveCohorts', () => {
+    it('is called on componentDidMount', () => {
+      const restore = Admin.prototype.retrieveCohorts
+      const mock = Admin.prototype.retrieveCohorts = jest.fn()
+      const wrapper = mount(<Router><Admin conn={fakeAxios} /></Router>)
+      expect(mock).toHaveBeenCalled()
+
+      Admin.prototype.retrieveCohorts = restore
+    })
+
+    it('returns data', async () => {
+      const admin = new Admin()
+      const data = await admin.retrieveCohorts(fakeAxios)
+      expect(data[0].name).toEqual("1610-BE")
+    })
   })
 })
